8f28fa4653575412ad6b548b7e9cda93ff6f5da8
Michal Baran <ambcontrol2014@gmail.com>
2020-07-31 17:21:43 +0200

    Finished section Enum

---
+++ Coffee Machine/Problems/Days of week/src/Main.java
@@ -0,0 +1,19 @@
+// declare your enum here
+
+public class Main {
+    public static void main(String[] args) {
+        for (DayOfWeek day : DayOfWeek.values()) {
+            System.out.println(day);
+        }
+    }
+
+    enum DayOfWeek {
+        MONDAY,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY,
+        SUNDAY;
+    }
+}
---
+++ Coffee Machine/Problems/The secret enumeration/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int count = 0;
+        Secret[] constants = Secret.values();
+
+        for (Secret value : constants) {
+            if (value.toString().startsWith("STAR")) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
+/*
+//At least two constants start with STAR
+enum Secret {
+    STAR, CRASH, START, STARSHIP, STA, FUN// ...
+}
+*/
