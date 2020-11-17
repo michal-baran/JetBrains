6c497d2686ddb67d6b9303fdf3eeeaa53059bca9
Michal Baran <ambcontrol2014@gmail.com>
2020-11-17 16:08:40 +0100

    Finished Reflection basics

--- Battleship/Problems/Square of a circle/src/Main.java
+++ Battleship/Problems/Square of a circle/src/Main.java
@@ -1,15 +1,10 @@
 
 
-class Circle implements Measurable {
+class Circle {
     private double radius;
 
     public Circle(double radius) {
         this.radius = radius;
-    }
-
-    @Override
-    public double square() {
-        return Math.PI * Math.pow(radius, 2.0);
     }
 }
 
